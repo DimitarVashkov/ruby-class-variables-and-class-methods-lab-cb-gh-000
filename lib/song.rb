eb8fa59cb800a7ac9b897da469f9809505451c33
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-25 23:52:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-
-    @@artists << @artist unless @@artists.include?(@artist)
-    @@genres << @genre unless @@genres.include?(@genre)
+    @@artists << @artist
+    @@genres << @genre
 
   end
 
